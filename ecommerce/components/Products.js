670782c80047ab4b5868b269753b9f307bd808a0
gale-666 <gale@azc.uam.mx>
2024-10-23 19:37:55 -0600

    ✨ Creando la vista de los productos

--- ecommerce/components/Products.js
+++ ecommerce/components/Products.js
@@ -1,21 +1,34 @@
 import React from 'react'
 import {data} from '../utils/data'
-import {Image} from '@chakra-ui/react'
+import {Box, Card, Grid, GridItem, Image, Link} from '@chakra-ui/react'
 // import {Image} from 'next/image'
 
 const Products = () => {
   return (
     <div>
-      {data.products.map((products) => (
-        <Image
-        key={products.id}
-        src={`/images${products.image}`}
-        alt={products.title}
-        width={230}
-        height={230}
-        style={{objectFit: "cover"}}
-        />
-      ))}
+      <Grid templateColumns={{base: "1fr", lg:"repeat(4,1fr)"}} gap={6}>
+        {data.products.map((products) => (
+          <GridItem>
+            <Card>
+              <Link href={`/product/${products.id}`}>
+              <Image
+              key={products.id}
+              src={`/images${products.image}`}
+              alt={products.title}
+              width={230}
+              height={230}
+              style={{objectFit: "cover"}}
+              />
+              <Box py={1} px={1}>
+                <h3>{products.title}</h3>
+                <div>{products.description}</div>
+                <div>${products.price}.00</div>
+              </Box>
+              </Link>
+            </Card>
+          </GridItem>
+        ))}
+      </Grid>
     </div>
   )
 }

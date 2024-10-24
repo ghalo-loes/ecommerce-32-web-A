02c5495b3076248e2498fcdabb1ac02d30b0b3e2
gale-666 <gale@azc.uam.mx>
2024-10-23 20:43:13 -0600

    ✨Creando la vista para cada producto

--- ecommerce/pages/product/[id].js
+++ ecommerce/pages/product/[id].js
@@ -1,18 +1,86 @@
 import React from 'react'
 import { useRouter } from 'next/router'
-import {data} from '../../utils/data'
+import { data } from '../../utils/data'
+import {
+    Container,
+    SimpleGrid,
+    Flex,
+    Image,
+    Heading,
+    Box,
+    Stack,
+    Button,
+    Text,
+    useColorModeValue
+} from '@chakra-ui/react'
 
 const ProductPage = () => {
     const router = useRouter() // Es un hook de next, que permite moverse entre rutas
-    const {id} = router.query // Extrae el parametro id
+    const { id } = router.query // Extrae el parametro id
     const product = data.products.find((product) => product.id === parseInt(id))
 
-    if(!product) {
+    if (!product) {
         return <div>Product not found</div>
     }
-  return (
-    <div>Product id - {product.id}</div>
-  )
+    return (
+        <Container maxW={'container.xl'} mt={2}>
+            <SimpleGrid columns={[1,2]} spacing={2}>
+                <Flex>
+                    <Image
+                    src={`../images${product.image}`}
+                    rounded={'md'}
+                    alt={product.title}
+                    fit={'cover'}
+                    align={'center'}
+                    h={'100%'}
+                    w={{base: '100%', sm:'400px', md:'500px', lg: '600px'}}
+                    />
+                </Flex>
+
+                <Stack spacing={{base: 6, md:10}}>
+                    <Box>
+                        <Heading
+                        fontWeight={600}
+                        fontSize={{base:'2xl', sm:'4xl', lg:'3xl'}}
+                        >
+                            {product.title}
+                        </Heading>
+                        <Text
+                        color={useColorModeValue('gray.900', 'gray.400')}
+                        fontWeight={300}
+                        fontSize={'2xl'}
+                        >
+                            $ {product.price} USD
+                        </Text>
+                    </Box>
+
+                    <Text
+                    color={useColorModeValue('gray.500', 'gray.400')}
+                    fontSize={'lg'}
+                    mb={'10'}
+                    >
+                        {product.description}
+                    </Text>
+
+                    <Flex alignItems={'end'}>
+                        <Button
+                        rounded={'md'}
+                        w={'full'}
+                        size={'lg'}
+                        py={'3'}
+                        bg={useColorModeValue('gray.900', 'gray.50')}
+                        color={useColorModeValue('white','gray.900')}
+                        textTransform={'uppercase'}
+                        _hover={{bg: 'green.400'}}
+                        _focus={{boxShadow: 'outline'}}
+                        >
+                            Add to cart
+                        </Button>
+                    </Flex>
+                </Stack>
+            </SimpleGrid>
+        </Container>
+    )
 }
 
 export default ProductPage
